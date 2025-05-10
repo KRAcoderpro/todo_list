36226ec844c917439306b7d28d66ea0983bb7f17
KRAcoderpro <kharubnfc73@gmail.com>
2025-05-10 16:32:45 +0300

    Fix notification task

--- backend/todo/tasks.py
+++ backend/todo/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 from .models import Task
 from .services import send_telegram_message
+from django.utils import timezone
 
 
 @shared_task
@@ -8,7 +9,10 @@ def notify_task(task_id):
     try:
         task = Task.objects.get(id=task_id)
 
-        if task.finished:
+        if task.finished or task.due_date is None:
+            return
+
+        if task.due_date > timezone.now():
             return
 
         send_telegram_message(
